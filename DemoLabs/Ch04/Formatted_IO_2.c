c2819b17a42d7ee96288c37d2c9c8c915cbf6a2e
DonMacary <dmacaryjr@gmail.com>
2018-07-12 12:30:52 -0500

    Edited a Demo lab to fix a bug

--- DemoLabs/Ch04/Formatted_IO_2.c
+++ DemoLabs/Ch04/Formatted_IO_2.c
@@ -5,7 +5,7 @@ int main(void)
 	char inputBuffer[256] = {0};
 
 	printf("Enter a string but don't use any capital letters. \n");
-	scanf("%255[^A-Z\n]s", inputBuffer);
+	scanf("%255[^A-Z\n]s", &inputBuffer);
 	printf("Your lower case string was: \n%s", inputBuffer);
 
 	return 0;
