4a7d5ba2a282290d9d82e8e193767e4fb25431d2
DonMacary <dmacaryjr@gmail.com>
2018-07-16 15:12:06 -0500

    Added Demo Labs for chapter 6, completed capstone1.c started perfLab6A but did not complete. HAVE NOT UPDATED README

---
+++ PerformanceLabs/Ch06-BitwiseOperations/perfLab6A.c
@@ -0,0 +1,31 @@
+/*********************************************************************
+/   AUTHOR: ELF
+/   COURSE: C Programming 
+/   PROGRAM NAME: perfLab6A.c
+/   PROGRAM DESCRIPTION: Performance Lab 6A - Shift Left
+/   INSTRUCTIONS:
+        Input a uint32_t userInput from stdin utilizing fscanf()
+        Initialize another uint32_t bitChecker to 0x01
+        Use a Bitwise Shift Left on bitChecker to ensure only the left most bit is turned on
+        Print the binary representation of userInput to stdout using only the following:
+            fprintf()
+            Bitwise Shift Right >>
+            Bitwise AND &
+            Relational Operator (e.g., >)
+/*********************************************************************/
+
+#include <stdio.h>
+
+int main (void)
+{
+    __uint32_t userInt;
+    __uint32_t bitChecker = 1;
+    printf("Please type an integer: ");
+    fscanf(stdin, "%u", &userInt);
+    printf("Your integer is %u (0x%.8X in hex)\n", userInt, userInt);
+    
+
+
+
+
+}
